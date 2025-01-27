683042ac708d600bcfd1a03015fcefbb575b060a
snlarsen <snlarsen@nlp-gpu-01.be.ucsc.edu>
2025-01-26 18:19:28 -0800

    yeah

--- test_transformer_BPE.sh
+++ test_transformer_BPE.sh
@@ -1,3 +1,4 @@
 fairseq-generate data-bin-transformer-BPE/iwslt13.tokenized.fr-en \
     --path checkpoints/checkpoint_best.pt \
-    --batch-size 128 --beam 5 --remove-bpe
+    --batch-size 128 --beam 5 --remove-bpe \
+    --gen-subset valid > valid.en.BPE.generated.txt
